45baa214f98cff9b29d855b24484d43589d10fe3
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2023-12-18 15:08:25 +0700

    Добавление NLog

--- ToDoListBlazor/Program.cs
+++ ToDoListBlazor/Program.cs
@@ -21,10 +21,16 @@ builder.Services.AddDbContext<ProblemContext>
 builder.Services.AddTransient<IProblem, ProblemManager>();
 builder.Services.AddControllersWithViews();
 
+builder.Logging.ClearProviders();
+builder.Logging.SetMinimumLevel(LogLevel.Trace);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
